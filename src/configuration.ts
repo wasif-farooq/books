e4fdb850329c0c4af8f5549962ca677364adbb8b
wasif <wasif@localhost.localdomain>
2020-07-15 23:30:56 +0500

    Implemented environments

--- src/configuration.ts
+++
@@ -1,6 +0,0 @@
-import { registerAs } from '@nestjs/config';
-export default registerAs('app', () => ({
-    db: process.env.DATABASE_URL,
-    name: process.env.APP_NAME,
-    port: process.env.APP_PORT,
-}));
